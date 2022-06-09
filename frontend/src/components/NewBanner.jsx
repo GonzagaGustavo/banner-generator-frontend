6f83eae272161e3557460b76efe3224257c3a6e1
romariojsx <romariojdosantos@gmail.com>
2022-06-09 15:51:00 -0300

    uploadImg

--- frontend/src/components/NewBanner.jsx
+++ frontend/src/components/NewBanner.jsx
@@ -1,16 +1,38 @@
 import React from "react";
+import axios from "axios";
 import "./NewBanner.css";
 
 function NewBanner() {
-  const [handleClick, setHandleClick] = React.useState();
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("selectedFile", selectedFile);
+    try {
+      const response = axios({
+        method: "post",
+        url: "/api/upload/file",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   return (
     <>
       <div className="files">
-        <input type="file" />
-        {}
-        <button style={{ width: "210px", marginLeft: "10px" }}>
-          Novo Banner
-        </button>
+        <input type="file" onChange={handleFileSelect} />
+        <input
+          type="submit"
+          value="Novo Banner"
+          style={{ width: "210px", marginLeft: "10px" }}
+        />
       </div>
     </>
   );
