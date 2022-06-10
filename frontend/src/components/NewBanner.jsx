d8ffc7efaa058a68173618936e38f0027b3f1874
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2022-06-10 09:39:22 -0300

    Upload de Imagem funcionando

--- frontend/src/components/NewBanner.jsx
+++ frontend/src/components/NewBanner.jsx
@@ -1,28 +1,22 @@
-import React, { useContext } from "react";
-import axios from "axios";
+import React from "react";
+import api from '../services/api'
 import "./NewBanner.css";
-import { Context } from "../services/Context";
 
 function NewBanner() {
-  const { setImg } = useContext(Context);
-
   const [selectedFile, setSelectedFile] = React.useState(null);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append("selectedFile", selectedFile);
-    setImg(formData);
-    try {
-      const response = axios({
-        method: "post",
-        url: "/api/upload/file",
-        data: formData,
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-    } catch (error) {
-      console.log(error);
+
+function upload() {
+  const formData = new FormData()
+  formData.append("image", selectedFile)
+  console.log(selectedFile)
+
+  const headers = {
+    'headers': {
+      'Content-Type': 'application/json'
     }
-  };
+  }
+  api.post("/upload", formData, headers)
+}
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -34,6 +28,7 @@ function NewBanner() {
         <input type="file" onChange={handleFileSelect} />
         <input
           type="submit"
+          onClick={upload}
           value="Novo Banner"
           style={{ width: "210px", marginLeft: "10px" }}
         />
