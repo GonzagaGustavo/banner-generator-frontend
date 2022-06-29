c90e8d01d6bd23e02ba3231c55d64129c1bcf112
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2022-06-29 14:26:00 -0300

    Função de sair

--- frontend/src/Layout/MenuDefault/SidebarItem/index.jsx
+++ frontend/src/Layout/MenuDefault/SidebarItem/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Container } from "./styles";
+import Cookies from 'js-cookie'
 
 const SidebarItem = ({ Icon, Text, Link }) => {
+function link() {
+  if(Link) {
+    window.location.href = Link
+  } else {
+    Cookies.remove("id")
+    Cookies.remove("token")
+    window.location.href = "/"
+  }
+}
   return (
-    <Container onClick={() => (window.location.href = Link)}>
+    <Container onClick={link}>
       <Icon />
       {Text}
     </Container>
