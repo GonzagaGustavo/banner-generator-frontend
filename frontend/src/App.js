32039560fded2e93f92612d0c129dc46493118fe
romariojsx <romariojdosantos@gmail.com>
2022-07-07 16:51:30 -0300

    span layout e login card center

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -168,49 +168,51 @@ function App() {
             {token ? <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} /> : <></>}
           </>
         ) : null}
-        {/* <span
-          style={{
-            padding: "20px",
-            width: "100%",
-            marginLeft: isOpen ? "20vw" : "5vw",
-            transition: "all 0.5s",
-          }}
-        > */}
         <Routes>
           <Route path="/" element={<Login />}></Route>
           {/* <Route path="/signup" element={<CreateUser />}></Route> Rota para excluir */}
           <Route path="/signup" element={<CreateAccount />}></Route>
         </Routes>
         {token ? (
-          <Routes>
-            <Route path="/admin/edit/:id" element={<EditUser />}></Route>
-            {/* <Route path="/painel/usuario" element={<Header />}></Route> Rota para exluir */}
-            <Route path="/admin/criacao" element={<NewUser />}></Route>
-            <Route path="/admin" element={<Template />}></Route>
-            <Route
-              path="/Banners"
-              element={
-                <Home
-                  sendXML={sendXML}
-                  buscar={buscar}
-                  setText={setText}
-                  text={text}
-                  setSelectedFile={setSelectedFile}
-                  dados={dados}
-                  e={e}
-                  aplicar={aplicar}
-                  apagar={apagar}
-                  download={download}
-                  downloadImage={downloadImage}
-                  selectedFile={selectedFile}
-                  selectedXML={selectedXML}
-                  setSelectedXML={setSelectedXML}
-                  linkXML={linkXML}
-                  setLinkXML={setLinkXML}
-                />
-              }
-            ></Route>
-          </Routes>
+          <span
+            id="span-container"
+            style={{
+              padding: "20px",
+              width: "100%",
+              marginLeft: isOpen ? "20vw" : "5vw",
+              transition: "all 0.5s",
+            }}
+          >
+            <Routes>
+              <Route path="/admin/edit/:id" element={<EditUser />}></Route>
+              {/* <Route path="/painel/usuario" element={<Header />}></Route> Rota para exluir */}
+              <Route path="/admin/criacao" element={<NewUser />}></Route>
+              <Route path="/admin" element={<Template />}></Route>
+              <Route
+                path="/Banners"
+                element={
+                  <Home
+                    sendXML={sendXML}
+                    buscar={buscar}
+                    setText={setText}
+                    text={text}
+                    setSelectedFile={setSelectedFile}
+                    dados={dados}
+                    e={e}
+                    aplicar={aplicar}
+                    apagar={apagar}
+                    download={download}
+                    downloadImage={downloadImage}
+                    selectedFile={selectedFile}
+                    selectedXML={selectedXML}
+                    setSelectedXML={setSelectedXML}
+                    linkXML={linkXML}
+                    setLinkXML={setLinkXML}
+                  />
+                }
+              ></Route>
+            </Routes>
+          </span>
         ) : (
           <>
             {location.pathname !== "/" && location.pathname !== "/signup" ? (
@@ -220,7 +222,6 @@ function App() {
             )}
           </>
         )}
-        {/* </span> */}
       </div>
     </>
   );
