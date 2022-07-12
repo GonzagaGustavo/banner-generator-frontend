8aa902f22510cceb29c349ad9f401bec7f46cf72
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2022-07-12 16:14:19 -0300

    Confirme excluir usuário

--- frontend/src/Views/SuperAdmin/Template.jsx
+++ frontend/src/Views/SuperAdmin/Template.jsx
@@ -12,7 +12,6 @@ function Template() {
   const [permition, setPermition] = useState(false);
   const token = Cookies.get("token");
   useEffect(() => {
-    
     if (token) {
       api.post("/users/", { token: token }).then((res) => {
         if (res.data === false) {
@@ -26,10 +25,10 @@ function Template() {
       setPermition(false);
     }
   }, []);
-function edit(dataIndex, rowIndex) {
-  const id = data[dataIndex].id
-  window.location.href = `/admin/edit/${id}`
-}
+  function edit(dataIndex, rowIndex) {
+    const id = data[dataIndex].id;
+    window.location.href = `/admin/edit/${id}`;
+  }
 
   const collouns = [
     {
@@ -62,7 +61,7 @@ function edit(dataIndex, rowIndex) {
       options: {
         filter: true,
         sort: true,
-      }
+      },
     },
     {
       name: "Editar",
@@ -73,57 +72,62 @@ function edit(dataIndex, rowIndex) {
         customBodyRenderLite: (dataIndex, rowIndex) => {
           return (
             <IconButton
-          className="iconButton"
-          onClick={() => edit(dataIndex, rowIndex)}
-        >
-          <MdEdit />
-        </IconButton>
+              className="iconButton"
+              onClick={() => edit(dataIndex, rowIndex)}
+            >
+              <MdEdit />
+            </IconButton>
           );
-        }
-      }
+        },
+      },
     },
   ];
   const options = {
     textLabels: {
       body: {
-          noMatch: "Nenhum registro encontrado",
-          toolTip: "Classificar",
+        noMatch: "Nenhum registro encontrado",
+        toolTip: "Classificar",
       },
       pagination: {
-          next: "Próxima página",
-          previous: "Página anterior",
-          rowsPerPage: "Linhas or página:",
-          displayRows: "de",
+        next: "Próxima página",
+        previous: "Página anterior",
+        rowsPerPage: "Linhas or página:",
+        displayRows: "de",
       },
       toolbar: {
-          search: "Busca",
-          downloadCsv: "Download CSV",
-          print: "Imprimir",
-          viewColumns: "Filtrar Colunas",
-          filterTable: "Filtrar Tabelas",
+        search: "Busca",
+        downloadCsv: "Download CSV",
+        print: "Imprimir",
+        viewColumns: "Filtrar Colunas",
+        filterTable: "Filtrar Tabelas",
       },
       filter: {
-          all: "Todos",
-          title: "FILTROS",
-          reset: "LIMPAR",
+        all: "Todos",
+        title: "FILTROS",
+        reset: "LIMPAR",
       },
       viewColumns: {
-          title: "Ver Colunas",
-          titleAria: "Ver/Esconder Colunas da Tabela",
+        title: "Ver Colunas",
+        titleAria: "Ver/Esconder Colunas da Tabela",
       },
       selectedRows: {
-          text: "registros(s) selecionados",
-          delete: "Excluir",
-          deleteAria: "Excluir registros selecionados",
+        text: "registros(s) selecionados",
+        delete: "Excluir",
+        deleteAria: "Excluir registros selecionados",
       },
-  },
+    },
     filterType: "checkbox",
     onRowsDelete: (rowsDeleted, dataRows) => {
-      const ids = rowsDeleted.data.map(d => data[d.dataIndex].id)
-      api.post("/users/delete", {ids: ids, token: token}).then(res => {
-        toast.success(res.data)
-      })
-    }
+      
+      if (window.confirm("Tem certeza que deseja deletar estes usuários?")) {
+        const ids = rowsDeleted.data.map((d) => data[d.dataIndex].id);
+        api.post("/users/delete", { ids: ids, token: token }).then((res) => {
+          toast.success(res.data);
+        });
+      } else {
+        window.location.reload()
+      }
+    },
   };
 
   return (
